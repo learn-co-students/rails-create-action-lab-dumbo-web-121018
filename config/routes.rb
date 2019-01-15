ddec953c1f63384fc7abfcfbcf9cb05481452b3c
rhuwellsamano <rhuwell.samano@gmail.com>
2019-01-14 23:21:12 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
   resources :students, only: [:index, :new, :create]
   get '/student/:id', to: 'students#show', as: 'student'
+
+  # get "/students", to: "students#index"
+  # get "/students/new", to: "students#new"
+  # post "/student", to: "students#create"
+
+  # get "/cats/:id", to: "cats#show", as: "cat"
+  
+  # get "/cats/:id/edit", to: "cats#edit", as: "edit_cat"
+  # patch "/cats/:id", to: "cats#update"
+  # delete "/cats/:id", to: "cats#destroy"
+
+
+
+
 end
